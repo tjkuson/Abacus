aa165f73d87c1a67ad7d63281f671c5732fb9d3f
Tom Kuson <12143269+tjkuson@users.noreply.github.com>
2022-12-25 17:23:49 +0000

    Remove ignored files

---
+++ config/settings/base.py
@@ -0,0 +1,110 @@
+"""
+Base settings to build other settings files upon.
+"""
+from pathlib import Path
+
+ROOT_DIR: Path = Path(__file__).resolve(strict=True).parent.parent.parent
+APPS_DIR: Path = ROOT_DIR / "abacus"
+
+# Time zone
+
+TIME_ZONE: str = "UTC"
+
+# Internationalization
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#language-code
+LANGUAGE_CODE: str = "en-gb"
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
+USE_I18N: bool = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
+USE_TZ: bool = True
+
+# Databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ROOT_DIR / "db.sqlite3",
+    }
+}
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
+
+
+# URLs
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
+ROOT_URLCONF = "config.urls"
+# https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Applications
+
+DJANGO_APPS: list[str] = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = []
+
+INSTALLED_APPS: list[str] = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Passwords
+
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Middleware
+
+MIDDLEWARE: list[str] = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# Static files
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL: str = "/static/"
+
+# Templates
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(APPS_DIR / "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
---
+++ config/urls.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.views import defaults as default_views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit these
+    # urls in the browser to see how these error pages look like.
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
